0f029d81b23d809b26abd3c59c0271634bcabff6
Diogo Carapito <43778648+DiogoCarapito@users.noreply.github.com>
2023-10-26 23:59:44 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,58 @@
+"""A setuptools based setup module.
 
+See:
+https://packaging.python.org/guides/distributing-packages-using-setuptools/
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+# Get the long description from the README file
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
+# Arguments marked as "Required" below must be included for upload to PyPI.
+# Fields marked as "Optional" may be commented out.
+
+setup(
+    name="mimufs",
+    version="0.0.1",
+    description="Mim@uf Business Intelligence Library for Portuguese Primary Care",  # Optional
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DiogoCarapito/mimufs",
+    author="DiogoCarapito",
+    author_email="diogo.carapito@gmail.com",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: Apache License 2.0",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+    keywords="business intelligence, primary care, data science, medicine",
+    packages=find_packages(where="mimufs"),
+    python_requires=">=3.7, <4",
+    install_requires=["pandas"],
+    # List additional URLs that are relevant to your project as a dict.
+    #
+    # This field corresponds to the "Project-URL" metadata fields:
+    # https://packaging.python.org/specifications/core-metadata/#project-url-multiple-use
+    #
+    # Examples listed include a pattern for specifying where the package tracks
+    # issues, where the source is hosted, where to say thanks to the package
+    # maintainers, and where to support the project financially. The key is
+    # what's used to render the link text on PyPI.
+    project_urls={  # Optional
+        "Bug Reports": "https://github.com/DiogoCarapito/mimufs/issues",
+        "Source": "https://github.com/DiogoCarapito/mimufs",
+    },
+)
