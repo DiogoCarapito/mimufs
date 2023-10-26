cadce598f72009e07a33273620c026088e4d5d2d
Diogo Carapito <43778648+DiogoCarapito@users.noreply.github.com>
2023-10-27 00:33:41 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,23 +5,23 @@ https://packaging.python.org/guides/distributing-packages-using-setuptools/
 https://github.com/pypa/sampleproject
 """
 
-# Always prefer setuptools over distutils
 from setuptools import setup, find_packages
 import pathlib
+import os
+import codecs
 
 here = pathlib.Path(__file__).parent.resolve()
 
-# Get the long description from the README file
-long_description = (here / "README.md").read_text(encoding="utf-8")
 
-# Arguments marked as "Required" below must be included for upload to PyPI.
-# Fields marked as "Optional" may be commented out.
+VERSION = "0.0.1"
+DESCRIPTION = "Mim@uf Business Intelligence Library for Portuguese Primary Care"
+LONG_DESCRIPTION = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="mimufs",
-    version="0.0.1",
-    description="Mim@uf Business Intelligence Library for Portuguese Primary Care",  # Optional
-    long_description=long_description,
+    version=VERSION,
+    description=DESCRIPTION
+    long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
     url="https://github.com/DiogoCarapito/mimufs",
     author="DiogoCarapito",
@@ -42,16 +42,7 @@ setup(
     packages=find_packages(where="mimufs"),
     python_requires=">=3.7, <4",
     install_requires=["pandas"],
-    # List additional URLs that are relevant to your project as a dict.
-    #
-    # This field corresponds to the "Project-URL" metadata fields:
-    # https://packaging.python.org/specifications/core-metadata/#project-url-multiple-use
-    #
-    # Examples listed include a pattern for specifying where the package tracks
-    # issues, where the source is hosted, where to say thanks to the package
-    # maintainers, and where to support the project financially. The key is
-    # what's used to render the link text on PyPI.
-    project_urls={  # Optional
+    project_urls={
         "Bug Reports": "https://github.com/DiogoCarapito/mimufs/issues",
         "Source": "https://github.com/DiogoCarapito/mimufs",
     },
