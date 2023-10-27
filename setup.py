2ba1f5a98ab549395554012d0317cd93838c709e
Diogo Carapito <43778648+DiogoCarapito@users.noreply.github.com>
2023-10-27 01:32:50 +0100

    setup.py

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Build Tools",
-        "License :: OSI Approved :: Apache License 2.0",
+        #"License :: OSI Approved :: Apache Software License 2.0",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
