eae67de8d20e12850ddfbfdd4a9c37fe3efeec9e
Diogo Carapito <43778648+DiogoCarapito@users.noreply.github.com>
2023-11-23 23:29:10 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import pathlib
 here = pathlib.Path(__file__).parent.resolve()
 
 
-VERSION = "0.0.21"
+VERSION = "0.0.22"
 
 DESCRIPTION = "Mim@uf Business Intelligence Library for Portuguese Primary Care"
 
@@ -31,7 +31,7 @@ setup(
     keywords="business intelligence, primary care, data science, medicine",
     python_requires=">=3.7,<3.12",
     install_requires=[],
-    #packages=find_packages(),
-    package_dir = {"": "src"},
+    packages=find_packages(),
+    #package_dir = {"": "src"},
     include_package_data=True,
 )
